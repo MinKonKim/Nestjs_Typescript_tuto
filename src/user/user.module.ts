c592f4975d7fc59b0a9d7c64dc9b20b2e39351cf
민곤 <alsrhs98@gmail.com>
2024-09-13 15:41:05 +0900

    해설7 5:23까지

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  import:[TypeOrmModule.forFeature([User])]
   controllers: [UserController],
   providers: [UserService],
 })
